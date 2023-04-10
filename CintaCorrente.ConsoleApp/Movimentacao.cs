c794479c4d3b60426316c02febeae4e13ce74967
Gustavo Rafaeli <gustavo.rafaeli.oliv@gmail.com>
2023-04-10 12:21:07 -0300

    Add project files.

---
+++ CintaCorrente.ConsoleApp/Movimentacao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrente.ConsoleApp
+{
+    public class Movimentacao
+    {
+        public double Valor { get; set; }
+        public string Tipo { get; set; }
+
+        public Movimentacao(double valor, string tipo)
+        {
+            Valor = valor;
+            Tipo = tipo;
+        }
+    }
+}
+
